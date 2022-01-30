d890a51c68c575130b4e13e55efbb665ebce44aa
Merhawi-gebreslasie <merhgebar@gmail.com>
2022-01-30 12:40:14 +0100

    modified

--- test1/main.js
+++ test1/main.js
@@ -1,15 +1,16 @@
 //exercise 1
 function logFunction(num){
-   if(num%3==0&&num%5==0){
-    console.log("Jackpot!");
-  }
-  else if(num%3==0){
-    console.log("This is divisible    by 3");
-  }
-  else if(num%5==0){
-    console.log("This is divisibleby 5");
+  for(let i=0;i<101;i++){
+
+    if(i%3==0&&i%5==0){
+     console.log("Jackpot!");
+   }
+   else if(i%3==0){
+     console.log("This is divisible    by 3");
+   }
+   else if(i%5==0){
+     console.log("This is divisibleby 5");
+   }
   }
 }
-logFunction(15)
-logFunction(3)
-console.log(5);
+logFunction()
